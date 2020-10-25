0a2e222ce603dca1b29f3f78d1e04bcc76546807
unknown <greatrl1118@gmail.com>
2020-10-26 05:20:38 +0800

    modified extractor

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -5,27 +5,14 @@ class Features(object):
     def __init__(self):
         self.vocab = []
     
-    def add_to_vocab(self, tweet):
-        processed_tweet = self.preprocess(tweet)
+    def add_to_vocab(self, processed_tweet):
         for word in processed_tweet:
             if not word in self.vocab:
                 self.vocab.append(word.lower())
 
-    def preprocess(self, tweet): 
-
-        signs = [",",".","!","?",";","...","\"","--","..","#","@"]
-        useless = ["and", "or","a","with","i","you","of","among","can","to","so","what","this","that","about","the","an","many","some","even","on","in","at","'s","-lrb-","-rrb-","are","`"]
-        for word in tweet:
-            if word in useless:
-                tweet.remove(word)
-                if word in signs:
-                    tweet.remove(word)
-        
-        return tweet
         
     
-    def extractor(self, tweet):
-        processed_tweet = self.preprocess(tweet)
+    def extractor(self, processed_tweet):
         ids = []
         for word in processed_tweet:
             if word.lower() in self.vocab:
